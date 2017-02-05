3b725c2dae990e7e742dfaf49c407a130297a5f1
Curtis Fenner <cwfenner@umich.edu>
2017-02-05 14:33:11 -0500

    Allow separate values and human visible options in dropdown

--- dropdown.pl
+++ dropdown.pl
@@ -9,11 +9,18 @@ our $_UNIQUE_PREFIX = "dropdownprefix";
 sub new {
 	my $class = shift;
 	my $options = shift;
+	my $values = shift || $options;
 	if (ref($options) ne 'ARRAY') {
 		warn("Dropdown expects an array-reference. You can use [] to create one");
 	}
+	if (ref($values) ne 'ARRAY') {
+		warn("Dropdown expects an array-reference. You can use [] to create one");
+	}
+	if (scalar @$options != scalar @$values) {
+		warn("Dropdown options and values arrays must be the same length");
+	}
 	$_index++;
-	my $obj = {'options' => $options, 'id' => $_UNIQUE_PREFIX . $_index};
+	my $obj = {'options' => $options, 'values' => $values, 'id' => $_UNIQUE_PREFIX . $_index};
 	return bless $obj, $class;
 }
 
@@ -24,16 +31,19 @@ sub html {
 	my $label = main::generate_aria_label($name);
 
 	# Get submitted value
-	my $value = $main::inputs_ref->{$name} || "";
+	my $submitted = $main::inputs_ref->{$name} || "";
 
 	# Output drop down
-	main::TEXT("<select class='pg-select' name='$name' id='$name' aria-label='$label' size='1' value='$value'>\n");
-	foreach my $option (@{$self->{'options'}}) {
+	main::TEXT("<select class='pg-select' name='$name' id='$name' aria-label='$label' size='1' value='$submitted'>\n");
+	for (my $i = 0; $i < scalar @{$self->{'options'}}; $i++) {
+		my $option = $self->{'options'}->[$i];
+		my $value = $self->{'values'}->[$i];
+
 		my $selected = '';
-		if ($option eq $value) {
+		if ($value eq $submitted) {
 			$selected = 'selected';
 		}
-		main::TEXT("<option value='$option' class='tex2jax_ignore' $selected>$option</option>\n");
+		main::TEXT("<option value='$value' class='tex2jax_ignore' $selected>$option</option>\n");
 	}
 	main::TEXT("</select>\n");
 
